e72055d546d9fe604dad68c4443c5cce177b8c1b
Karthi3002 <164326735+Karthi3002@users.noreply.github.com>
2025-01-02 16:36:16 +0530

    commit2

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,11 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"auction-bazar-db","appId":"1:570787756626:web:0bd461a22aa2b4a1243d8b","storageBucket":"auction-bazar-db.firebasestorage.app","apiKey":"AIzaSyAZh8itdyLPbmDangQ2c8yIKuGrxaYrxVI","authDomain":"auction-bazar-db.firebaseapp.com","messagingSenderId":"570787756626"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase())]
 };
